1560e9898c591eba8dc122914e61f6fd8efdef2d
Petr Viktorin <encukou@gmail.com>
2012-12-16 23:38:58 +0100

    Cache updating

--- wikibot/wikicache.py
+++ wikibot/wikicache.py
@@ -24,7 +24,7 @@ class WikiCache(object):
     :param limit: The cache will not make more than one request each `limit`
         seconds.
     """
-    def __init__(self, url_base, db_url=None, limit=5):
+    def __init__(self, url_base, db_url=None, force_sync=False, limit=5):
         if db_url is None:
             db_url = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                 'wikicache.sqlite')
@@ -42,7 +42,15 @@ class WikiCache(object):
         self._needed_metadata = set()
         self._needed_pages = set()
 
-        self.update()
+        self.update(force_sync=force_sync)
+
+        gevent.spawn(self._update_loop)
+
+    def _update_loop(self):
+        while True:
+            gevent.sleep(60)
+            self.log('Updating!')
+            self.update(force_sync=True)
 
     def get_wiki(self, session):
         query = session.query(cacheschema.Wiki).filter_by(
@@ -99,12 +107,18 @@ class WikiCache(object):
         params['format'] = 'yaml'
         return yaml.load(self._apirequest_raw(**params))
 
-    def update(self):
+    def update(self, force_sync=True):
         """Fetch a batch of page changes from the server"""
         session = self.make_session()
         wiki = self.get_wiki(session)
+        if wiki.last_update and not force_sync:
+            thresh = datetime.datetime.today() - datetime.timedelta(minutes=5)
+            if wiki.last_update > thresh:
+                self.log('Skipping update (last update was {})'.format(
+                    wiki.last_update))
+                return
         if wiki.sync_timestamp is None:
-            self.log('Initial update')
+            self.log('Initial cache setup')
             feed = self.apirequest(action='query', list='recentchanges',
                     rcprop='timestamp', rclimit=1)
             last_change = feed['query']['recentchanges'][0]
@@ -112,9 +126,63 @@ class WikiCache(object):
             wiki.synced = True
             self.invalidate_cache(session)
             session.commit()
+        else:
+            self.log('Updating cache')
+            feed = self.apirequest(action='query', list='recentchanges',
+                    rcprop='title|user|timestamp', rclimit=100,
+                    rcend=wiki.sync_timestamp
+                )
+            sync_timestamp = feed['query']['recentchanges'][0]['timestamp']
+            while feed:
+                invalidated = set()
+                changes = feed['query']['recentchanges']
+                for change in changes:
+                    title = change['title']
+                    if title not in invalidated:
+                        self.log(u'Change to {0} by {1}'.format(title,
+                                change['user']))
+                        obj = self._page_object(session, wiki, title)
+                        obj.up_to_date = False
+                        invalidated.add(title)
+                session.commit()
+                try:
+                    feed = self.apirequest(action='query', list='recentchanges',
+                            rcprop='title|user|timestamp', rclimit=100,
+                            rcend=wiki.sync_timestamp,
+                            **feed['query-continue']['recentchanges']
+                        )
+                except KeyError:
+                    feed = None
+                    wiki.sync_timestamp = sync_timestamp
+                    wiki.synced = True
+                else:
+                    wiki.synced = False
+                session.commit()
+        wiki.last_update = datetime.datetime.today()
+        session.commit()
 
     def _page_query(self, session):
         return session.query(cacheschema.Page)
+
+    def _page_object(self, session, wiki, title):
+        """Get an object for the page 'title', *w/o* adding it to the session
+        """
+        title = self.normalize_title(title)
+        obj = self._page_query(session).get((self.url_base, title))
+        if obj:
+            return obj
+        else:
+            obj = cacheschema.Page()
+            obj.wiki = wiki
+            obj.title = title
+            obj.revision = 0
+            obj.up_to_date = False
+            return obj
+
+    def normalize_title(self, title):
+        # TODO: http://www.mediawiki.org/wiki/API:Query#Title_normalization
+        title = title.replace('_', ' ')
+        return title[0].upper() + title[1:]
 
     def invalidate_cache(self, session):
         """Invalidate the entire cache
